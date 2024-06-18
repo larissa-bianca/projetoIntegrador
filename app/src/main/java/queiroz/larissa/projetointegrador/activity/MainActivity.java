7c667faf313400369181cadbebd94cdbec102656
Larissa <larilbpq.2005@gmail.com>
2024-06-18 14:48:52 -0300

    timePicker Dialog DatePicker Dialog interface do comp 1 interface de new item completa

--- app/src/main/java/queiroz/larissa/projetointegrador/activity/MainActivity.java
+++ app/src/main/java/queiroz/larissa/projetointegrador/activity/MainActivity.java
@@ -12,19 +12,9 @@ import queiroz.larissa.projetointegrador.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    static int NEW_ITEM_REQUEST = 1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FloatingActionButton fabAddAlarm = findViewById(R.id.fabAddNewAlarm);
-        fabAddAlarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,NewItemActivity.class);
-                startActivityForResult(i, NEW_ITEM_REQUEST);
-            }
-        });
     }
 }
--- app/src/main/java/queiroz/larissa/projetointegrador/activity/NewItemActivity.java
+++ app/src/main/java/queiroz/larissa/projetointegrador/activity/NewItemActivity.java
@@ -1,11 +1,19 @@
 package queiroz.larissa.projetointegrador.activity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.TimePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -30,6 +38,66 @@ public class NewItemActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        TextView tvHora = findViewById(R.id.tvHora);
+
+        ImageButton imgButtonTime = findViewById(R.id.imgButtonTime);
+        imgButtonTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // on below line we are getting the
+                // instance of our calendar.
+                final Calendar c = Calendar.getInstance();
+
+                // on below line we are getting our hour, minute.
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+
+                // on below line we are initializing our Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(NewItemActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+                                // on below line we are setting selected time
+                                // in our text view.
+                                tvHora.setText(hourOfDay + ":" + minute);
+                            }
+                        }, hour, minute, false);
+                // at last we are calling show to
+                // display our time picker dialog.
+                timePickerDialog.show();
+            }
+        });
+
+        TextView tvDate = findViewById(R.id.tvDate);
+
+        ImageButton imgBtnData = findViewById(R.id.imgBtnData);
+        imgBtnData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get Current Date
+                final Calendar c = Calendar.getInstance();
+                int Year = c.get(Calendar.YEAR);
+                int Month = c.get(Calendar.MONTH);
+                int Day = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(NewItemActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                tvDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, Year, Month, Day);
+                datePickerDialog.show();
+            }
+        });
+
         Button btnAddItem = findViewById(R.id.btnAddItem);
         btnAddItem.setOnClickListener(new View.OnClickListener() {
             @Override
