866d7adbebafaef9d54abd63d2260590412030a6
Duda <eduardarmour@gmail.com>
2024-06-24 14:39:05 -0300

    mudança vm.abrirCaixa()

--- app/src/main/java/queiroz/larissa/projetointegrador/model/MainActivityViewModel.java
+++ app/src/main/java/queiroz/larissa/projetointegrador/model/MainActivityViewModel.java
@@ -100,6 +100,44 @@ public class MainActivityViewModel extends AndroidViewModel {
         return result;
     }
 
+    public LiveData<Boolean> abrirCaixa(int caixa) {
+
+        // Cria um container do tipo MutableLiveData (um LiveData que pode ter seu conteúdo alterado).
+        MutableLiveData<Boolean> result = new MutableLiveData<>();
+
+        // Cria uma nova linha de execução (thread). O android obriga que chamadas de rede sejam feitas
+        // em uma linha de execução separada da principal.
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        // Executa a nova linha de execução. Dentro dessa linha, iremos realizar as requisições ao
+        // servidor web.
+        executorService.execute(new Runnable() {
+
+            /**
+             * Tudo o que colocármos dentro da função run abaixo será executada dentro da nova linha
+             * de execução.
+             */
+            @Override
+            public void run() {
+
+                // Criamos uma instância de Repository. É dentro dessa classe que estão os
+                // métodos que se comunicam com o ESP32.
+                Repository repository = new Repository(getApplication());
+
+                // O método turnLedOff envia uma requisição ao ESP32 pedindo que ele desligue o LED. Ele
+                // retorna um booleano indicando true caso o ESP32 tenha realizado a ação e
+                // false em caso contrário
+                boolean b = repository.abrirCaixa(caixa);
+
+                // Aqui postamos o resultado da operação dentro do LiveData. Quando fazemos isso,
+                // quem estiver observando o LiveData será avisado de que o resultado está disponível.
+                result.postValue(b);
+            }
+        });
+
+        return result;
+    }
+
     /**
      * Método que cria e executa uma requisição ao servidor web para pegar o status do LED
      * @return um LiveData que vai conter a resposta do servidor quando esta estiver disponível
