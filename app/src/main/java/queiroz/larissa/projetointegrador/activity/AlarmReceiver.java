ef366b99162989f4b6e0a038d4836e04a4f5d5ea
Larissa <larilbpq.2005@gmail.com>
2024-07-02 16:56:27 -0300

    AlarmReceiver

--- app/src/main/java/queiroz/larissa/projetointegrador/activity/AlarmReceiver.java
+++ app/src/main/java/queiroz/larissa/projetointegrador/activity/AlarmReceiver.java
@@ -1,5 +1,7 @@
 package queiroz.larissa.projetointegrador.activity;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -23,15 +25,8 @@ public class AlarmReceiver extends BroadcastReceiver {
         vibrator.vibrate(4000);
 
         Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if (alarmUri == null) {
-            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        }
 
-        // setting default ringtone
-        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
-
-        // play ringtone
-        ringtone.play();
+        Intent i = new Intent(AlarmReceiver.this, NewItemActivityAlarm.class);
+        startActivity(i.this);
     }
 }
